06727bf5c6c58fc3ec719fc0bdbb87d446e3fed1
HanningtoneM <hanningtonepompyy@gmail.com>
2022-08-17 16:25:17 +0300

    Fix linter

--- decode.rb
+++ decode.rb
@@ -1,28 +1,25 @@
- def morse_code(char)
-    code = {
-      'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.',
-      'F' => '..-.', 'G' => '--.', 'H' => '....',
-      'I' => '..', 'J' => '.---', 'K' => '-.-', 'L' => '.-..',
-      'M' => '--', 'N' => '-.', 'O' => '---', 'P' => '.--.',
-      'Q' => '--.-', 'R' => '.-.', 'S' => '...', 'T' => '-',
-      'U' => '..-', 'V' => '...-', 'W' => '.--', 'X' => '-..-',
-      'Y' => '-.--', 'Z' => '--..'
-    }
-    code.key(char)
-  end
+def morse_code(char)
+  code = {
+    'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.',
+    'F' => '..-.', 'G' => '--.', 'H' => '....',
+    'I' => '..', 'J' => '.---', 'K' => '-.-', 'L' => '.-..',
+    'M' => '--', 'N' => '-.', 'O' => '---', 'P' => '.--.',
+    'Q' => '--.-', 'R' => '.-.', 'S' => '...', 'T' => '-',
+    'U' => '..-', 'V' => '...-', 'W' => '.--', 'X' => '-..-',
+    'Y' => '-.--', 'Z' => '--..'
+  }
+  code.key(char)
+end
 
-  def decode_msg(msg)
-    array = msg.split('   ')
-    english = []
-    array.each { |word| english.insert(english.length, decode_word(word)) }
-    english.join(' ')
-  end
+def decode_msg(msg)
+  array = msg.split('   ')
+  english = []
+  array.each { |word| english.insert(english.length, decode_word(word)) }
+  english.join(' ')
+end
 
-  puts morse_code('.-')
+puts morse_code('.-')
 
-  puts decode_word('-- -.--   -. .- -- .')
+puts decode_word('-- -.--   -. .- -- .')
 
-  puts decode_msg('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
-
-
- 
+puts decode_msg('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
